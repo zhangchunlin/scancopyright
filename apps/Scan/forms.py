f1a1a6164ab69c5b16ac83953c9577e8432a56d7
zhangclb <zhangclb@lenovo.com>
2015-08-17 11:10:10 +0800

    fix import error

--- apps/Scan/forms.py
+++ apps/Scan/forms.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 #coding=utf-8
 
-from uliweb.form import Form,RadioSelectField,IS_IN_SET,Submit,BooleanField,TextField
+from uliweb.form import Form,RadioSelectField,Submit,BooleanField,TextField
 from uliweb.i18n import ugettext as _
 
 class CrconflictForm(Form):
@@ -11,10 +11,10 @@ class CrconflictForm(Form):
 SEARCH_CHOICES = [('0', '*'), ('1', 'True'),('2','False')]
 class CopyrightSearchForm(Form):
     form_buttons = Submit(value='search files')
-    c = RadioSelectField(name='fc', id='fc', choices=SEARCH_CHOICES, validators=[IS_IN_SET(SEARCH_CHOICES)], default='0', label = _('Having Copyright'),html_attrs={'_class':'radio inline'},)
-    ci = RadioSelectField(name='fci', id='fci', choices=SEARCH_CHOICES, validators=[IS_IN_SET(SEARCH_CHOICES)], default='0', label = _('Copyright Inhouse'),html_attrs={'_class':'radio inline'},)
-    cg = RadioSelectField(name='fcg', id='fcg', choices=SEARCH_CHOICES, validators=[IS_IN_SET(SEARCH_CHOICES)], default='0', label = _('Copyright GPL'),html_attrs={'_class':'radio inline'},)
-    co = RadioSelectField(name='fco', id='fco', choices=SEARCH_CHOICES, validators=[IS_IN_SET(SEARCH_CHOICES)], default='0', label = _('Copyright Other Opensource'),html_attrs={'_class':'radio inline'},)
+    c = RadioSelectField(name='fc', id='fc', choices=SEARCH_CHOICES, default='0', label = _('Having Copyright'),html_attrs={'_class':'radio inline'},)
+    ci = RadioSelectField(name='fci', id='fci', choices=SEARCH_CHOICES, default='0', label = _('Copyright Inhouse'),html_attrs={'_class':'radio inline'},)
+    cg = RadioSelectField(name='fcg', id='fcg', choices=SEARCH_CHOICES, default='0', label = _('Copyright GPL'),html_attrs={'_class':'radio inline'},)
+    co = RadioSelectField(name='fco', id='fco', choices=SEARCH_CHOICES, default='0', label = _('Copyright Other Opensource'),html_attrs={'_class':'radio inline'},)
 
 class ReleaseForm(Form):
     release = BooleanField(name='release_bool',id='release_bool',label = _('Release this package'))
